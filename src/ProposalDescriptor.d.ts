94511b748fc6bc9d51562592e8b448ca74bd9b2f
Arturas <a.molcanovas@gmail.com>
2019-09-02 15:57:48 +0100

    feat: Allow decorating static methods

--- src/ProposalDescriptor.d.ts
+++ src/ProposalDescriptor.d.ts
@@ -2,13 +2,13 @@
 export interface ProposalDescriptor extends PropertyDescriptor {
   descriptor: ProposalDescriptor; // Babel impl
 
-  extras: Partial<ProposalDescriptor>[];
+  extras?: Partial<ProposalDescriptor>[];
 
   key: string;
 
   kind: string;
 
-  method: Function;
+  method?: Function;
 
   placement: string;
 
